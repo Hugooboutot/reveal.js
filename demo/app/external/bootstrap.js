abc5378f9dc999bf46138768084a22dc007dea64
Christophe Porteneuve <tdd@tddsworld.com>
2015-01-20 09:10:01 +0100

    Purge contenus spécifiques pres BLEND

--- demo/app/external/bootstrap.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-require('./bootstrap/modal');
-require('./bootstrap/tooltip');
-require('./bootstrap/transition');
--- demo/app/initialize.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-window.jQuery = require('jquery');
-require('bootstrap');
-require('backbone').$ = window.jQuery;
-delete window.jQuery;
-require('backbone-mediator');
-
-require('application');
